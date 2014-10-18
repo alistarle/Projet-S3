e91f9ba2eff84e2a80ad4c6165dbfa1a0665d105
Amaury <amaury.dumetz@yahoo.fr>
2014-10-18 23:25:16 +0200

    J'ai commité pour la 2ème fois 8-)

--- Projet-S3/src/Start.java
+++ Projet-S3/src/Start.java
@@ -1,8 +1,46 @@
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
-public class Start {
+import javax.swing.JFileChooser;
+import javax.swing.UIManager;
+import javax.swing.filechooser.FileFilter;
 
-	public static void main(String[] args) {
-		System.out.println("GG Vous avez réussi à récupéré le dernier commit !");
-	}
+class Start {
 	
+	// Un joli JFileChooser pompé sur le net (ça en ouvre 2 ya un pb)
+	
+    public static void main(String[] arg) throws IOException {
+    	
+    	
+	
+	FileFilter java = new FiltreSimple("Fichiers Java",".java");
+	FileFilter classes = new FiltreSimple("Fichiers Class",".class");
+	FileFilter text= new FiltreSimple("Fichiers Text",".txt");
+	FileFilter jar = new FiltreSimple("Fichiers JAR",".jar");
+	
+	
+		JFileChooser dialogue = new JFileChooser(new File("."));
+		dialogue.addChoosableFileFilter(java);
+		dialogue.addChoosableFileFilter(classes);
+		dialogue.addChoosableFileFilter(jar);
+		dialogue.addChoosableFileFilter(text);
+		dialogue.showOpenDialog(null);
+		
+		
+		PrintWriter sortie;
+		File fichier;
+		
+		if (dialogue.showOpenDialog(null)== 
+		    JFileChooser.APPROVE_OPTION) {
+			
+			
+		    fichier = dialogue.getSelectedFile();
+		    sortie = new PrintWriter
+			(new FileWriter(fichier.getPath(), true));
+		    sortie.println(arg[0]);
+		    sortie.close();
+		}
+    }
 }
